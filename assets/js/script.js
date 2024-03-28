abd5b264aa5974b7b0010375e9c98407999b5ccd
Vendula Martinková <vendaciki@seznam.cz>
2024-03-28 18:36:45 +0100

    wendy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,12 @@
 $(document).ready(function() {
     // Data pro první sekci menu
-    const firstSectionData = [
-        { label: "Literární druhy", href: "../html/lit-druhy.html", iconClass: "fas fa-book" },
-        { label: "Literární žánry", href: "../html/lit-zanry.html", iconClass: "fas fa-bookmark" },
+    const Data = [
+        { label: "Literární druhy", href: "../html/lit-druh.html", iconClass: "fas fa-book" },
+        { label: "Literární žánry", href: "../html/lit-zanr.html", iconClass: "fas fa-bookmark" },
+        { label: "Literární směry", href: "../html/lit-smer.html", iconClass: "fas fa-route" },
+
         { label: "Rozbor díla", href: "../html/rozbor.html", iconClass: "fas fa-edit" },
         
-    ];
-
-    // Data pro druhou sekci menu
-    const secondSectionData = [
-        { label: "Starověká literatura", href: "../html/starovek.html", iconClass: "fas fa-book-reader" },
-        { label: "Antická literatura", href: "../html/antika.html", iconClass: "fas fa-theater-masks" },
-        { label: "Středověká literatura", href: "../html/stredovek.html", iconClass: "fas fa-feather-alt" },
-        { label: "Renesance a Humanismus", href: "../html/renesance.html", iconClass: "fas fa-palette" },
-        { label: "Baroko", href: "../html/baroko.html", iconClass: "fas fa-mask" },
-        { label: "Klasicismus", href: "../html/klasicismus.html", iconClass: "fas fa-archway" },
-        { label: "Osvícenství", href: "../html/osvicenstvi.html", iconClass: "fas fa-lightbulb" },
-        { label: "Preromantismus", href: "../html/preromantismus.html", iconClass: "fas fa-magic" },
-        { label: "Národní obrození", href: "../html/obrozeni.html", iconClass: "fas fa-flag" },
-        { label: "Romantismus", href: "../html/romantismus.html", iconClass: "fas fa-dove" },
-        { label: "Realismus", href: "../html/realismus.html", iconClass: "fas fa-book-open" },
-        { label: "Socialistický realismus", href: "../html/socialismus.html", iconClass: "fas fa-user" },
-        { label: "Naturalismus", href: "../html/naturalismus.html", iconClass: "fas fa-search" },
-        { label: "Česká lit. 2. poloviny 19. století", href: "../html/ceska19stol.html", iconClass: "fas fa-glasses" },
-        { label: "Literární moderna", href: "../html/moderna.html", iconClass: "fas fa-paper-plane" },
-        { label: "Literatura 1. pol. 20. století", href: "../html/1pol20stol.html", iconClass: "fas fa-marker" },
-        { label: "Literatura 2. pol. 20. století", href: "../html/2pol20stol.html", iconClass: "fas fa-marker" }
     ];
 
     // Funkce pro vytvoření položek menu
@@ -53,13 +34,9 @@ $(document).ready(function() {
     const menuItemsContainer = document.getElementById('menu-items');
 
     // Vytvoření první sekce menu
-    createMenuItems(firstSectionData, menuItemsContainer);
-    
-    // Přidání oddělovače mezi první a druhou sekci
-    const hrElement = document.createElement('hr');
-    menuItemsContainer.appendChild(hrElement);
+    createMenuItems(Data, menuItemsContainer);
 
     // Vytvoření druhé sekce menu
-    createMenuItems(secondSectionData, menuItemsContainer);
+    createMenuItems( menuItemsContainer);
    
 });    
